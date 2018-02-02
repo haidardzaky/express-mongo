a5c13c8670bd1619ded853b0e38e263c625b9979
haidardzaky <haidardz@gmail.com>
2018-02-02 23:38:29 +0700

    add 1st files

---
+++ public/javascripts/global.js
@@ -0,0 +1,22 @@
+// Userlist data array for filling in info box
+
+var userDataList = [];
+
+// DOM ready
+$(document).ready(function(){
+
+  //popolate the user table on initial page load
+  populateTable();
+})
+
+//Functions =====
+
+//Fill Table with new data
+
+function populateTable(){
+
+  //empty content string
+  var tableContent = '';
+
+  
+}
