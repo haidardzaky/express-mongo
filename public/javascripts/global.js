1fd4d42dce34a980c2150a66013d4732496f4bf8
haidardzaky <haidardz@gmail.com>
2018-02-04 23:08:25 +0700

    update data

--- public/javascripts/global.js
+++ public/javascripts/global.js
@@ -3,7 +3,7 @@
 var userDataList = [];
 
 // DOM ready
-$(document).ready(function(){
+$(document).ready(function() {
 
   //popolate the user table on initial page load
   populateTable();
@@ -13,10 +13,59 @@ $(document).ready(function(){
 
 //Fill Table with new data
 
-function populateTable(){
+function populateTable() {
 
   //empty content string
   var tableContent = '';
 
-  
-}
+  //jQuery AJAX call for JSON
+  $.getJSON('/users/userlist', function(data) {
+
+    // For each item in our JSON, add a table row and cells to the content string
+
+    // Stick our user data array into a userlist variable in the global object
+    userListData = data;
+
+    $.each(data, function() {
+      tableContent += '<tr>';
+      tableContent += '<td><a href="#" class="linkshowuser" rel="' + this.username + '">' + this.username + '</a></td>';
+      tableContent += '<td>' + this.email + '</td>';
+      tableContent += '<td><a href="#" class="linkdeleteuser" rel="' + this._id + '">delete</a></td>';
+      tableContent += '</tr>';
+
+      // Inject the whole content string into our existing HTML table
+      $('#userList table tbody').html(tableContent);
+    });
+
+  });
+};
+
+
+// Show User Info
+function showUserInfo(event) {
+
+  // Prevent Link from Firing
+  event.preventDefault();
+
+  // Retrieve username from link rel attribute
+  var thisUserName = $(this).attr('rel');
+
+  // Get Index of object based on id value
+  var arrayPosition = userListData.map(function(arrayItem) {
+    return arrayItem.username;
+  }).indexOf(thisUserName);
+
+  // Get our User Object
+  var thisUserObject = userListData[arrayPosition];
+
+  //Populate Info Box
+  $('#userInfoName').text(thisUserObject.fullname);
+  $('#userInfoAge').text(thisUserObject.age);
+  $('#userInfoGender').text(thisUserObject.gender);
+  $('#userInfoLocation').text(thisUserObject.location);
+
+};
+
+
+// Username link click
+$('#userList table tbody').on('click', 'td a.linkshowuser', showUserInfo);
